b45ac758a0dcab7c19252803204f6ee7b7d20ce1
Lovish Duggal <lovishduggal11@gmail.com>
2024-06-13 14:51:32 +0530

    star rating

---
+++ star-rating/index.js
@@ -0,0 +1,43 @@
+const starList = document.querySelectorAll('.star');
+let flag = true;
+
+for (let index = 0; index < starList.length; index++) {
+    starList[index].addEventListener('click', (e) => {
+        flag = !flag;
+        const idx = e.target.dataset.index;
+
+        for (let index = 0; index < starList.length; index++) {
+            starList[index].style.color = 'black';
+        }
+
+        for (let index = 0; index < idx; index++) {
+            starList[index].style.color = 'yellow';
+        }
+    });
+
+    starList[index].addEventListener('dblclick', (e) => {
+        flag = !flag;
+        const idx = e.target.dataset.index;
+        starList[idx].style.color = 'black';
+    });
+
+    starList[index].addEventListener('mouseover', (e) => {
+        const idx = e.target.dataset.index;
+        for (let index = 0; index < starList.length; index++) {
+            starList[index].style.color = 'black';
+        }
+
+        for (let index = 0; index < idx; index++) {
+            starList[index].style.color = 'yellow';
+        }
+    });
+
+    starList[index].addEventListener('mouseout', (e) => {
+        const idx = e.target.dataset.index;
+        if (flag && idx < 2) {
+            for (let index = 0; index < starList.length; index++) {
+                starList[index].style.color = 'black';
+            }
+        }
+    });
+}
